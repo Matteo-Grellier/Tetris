2289173f66b19e173dfcf8c09ea1f89de99a506c
matheoleger <71370253+matheoleger@users.noreply.github.com>
2021-12-09 15:04:01 +0100

    Update delay system + fix first error of Difficulties

--- Game.cs
+++ Game.cs
@@ -14,7 +14,7 @@ public class Game {
     public static int height { get => _height; set {_height = value;}}
 
     public static Grid grid;
-    public static int difficultyDelay;
+    public static int difficultyDelay = 1000;
     public static Score score;
     public static int choosenDifficulty = 0;
     public static int delay;
@@ -27,7 +27,7 @@ public class Game {
 
         Piece.InitShapes(); //il faudra absolument réinitialisé le tableau (car quand on recharge la page, cette méthode est appelé) 
         
-
+        delay = difficultyDelay;
 
         score = new Score(choosenDifficulty);
 
@@ -37,14 +37,73 @@ public class Game {
         // Score.score = 0;
     }
 
-    public static async Task Round() { //il faudra trouver une autre solution pour le Delay
+    // public static async Task Round() { //il faudra trouver une autre solution pour le Delay
+
+    //     Console.WriteLine("I launch the game !");
+
+    //     // var delay = 1000;
+
+    //     while(!isEnd) {
+    //         await Task.Delay(delay);
+
+    //         int offsetX = 0;
+    //         int offsetY = 1;
+    //         int offsetRotation = 0;
+
+    //         grid.RemovePiece(); //on enlève la pièce actuelle pour vérifier la nouvelle position (pour éviter les conflits).
+
+    //         bool isMovable = grid.VerifyNewPosition(offsetX, offsetY, offsetRotation);
+
+    //         if(!isMovable) {
+
+
+    //             Game.score.End(grid.actualPiece.y);
+
+    //             Game.delay = difficultyDelay;
+    //             Game.score.drop = "null";
+
+                
+    //             grid.AddToGrid(0, 0, 0); //on ajoute la pièce actuel à sa dernière position (avant de mettre la prochaine pièce)
+    //             grid.IsCompleteLine();
+
+    //             grid.actualPiece = grid.nextPiece;
+    //             grid.nextPiece = new Piece(grid.width, 2);
+
+    //             offsetY = 0;
+    //             offsetX = 0;
+
+    //             // Console.WriteLine("Fin de la pièce, début de la nouvelle");
+    //         } else {
+    //             offsetY = 1;
+    //             offsetX = 0;
+
+
+    //             // Console.WriteLine("nouvelle position pour la pièce !");
+    //             // Console.WriteLine("size : " + grid.pieces.Count);
+    //             // Console.WriteLine("x and y : " + grid.actualPiece.x + " " + grid.actualPiece.y);
+    //         }
+
+    //         //ici
+    //         bool isPosable = grid.VerifyNewPosition(offsetX, offsetY, offsetRotation);
+
+    //         if (isPosable){
+    //             grid.AddToGrid(offsetX, offsetY, offsetRotation); //on ajoute la pièce à sa nouvelle position.
+
+    //         } else {
+    //             isEnd = true; 
+    //             Console.WriteLine("HAHA NOOB YOU FAILLLLLLED");
+
+    //         }
+            
+
+    //     }
+    // }
+
+    public static void Round() { //il faudra trouver une autre solution pour le Delay
 
         Console.WriteLine("I launch the game !");
 
         // var delay = 1000;
-
-        while(!isEnd) {
-            await Task.Delay(delay);
 
             int offsetX = 0;
             int offsetY = 1;
@@ -94,9 +153,6 @@ public class Game {
                 Console.WriteLine("HAHA NOOB YOU FAILLLLLLED");
 
             }
-            
-
-        }
     }
     
 }

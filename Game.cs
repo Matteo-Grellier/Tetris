2509d433c6e5ad6e779148bcb4402b80ddfd2a3f
matheoleger <71370253+matheoleger@users.noreply.github.com>
2021-12-09 23:49:09 +0100

    Add nextPiece displaying + Fix Game Over menu + Retry game
    J'ai ajouté l'affichage de la prochaine pièce sur le côté (de manière basique...
    J'ai aussi réintégré le Menu du Game Over, qui avait été enlevé par erreur. Je l'ai amélioré en y ajoutant un bouton 'back' et un bouton 'retry'.
    J'ai donc aussi ajouté une fonctionnalité pour recommencer une partie, via ce bouton ou via le menu en recliquant sur Play.

--- Game.cs
+++ Game.cs
@@ -14,6 +14,8 @@ public class Game {
     public static int height { get => _height; set {_height = value;}}
 
     public static Grid grid;
+
+    public static Grid nextPieceGrid;
     public static int difficultyDelay = 1000;
     public static Score score;
     public static int choosenDifficulty = 0;
@@ -25,79 +27,23 @@ public class Game {
     public static void Init() {
         Console.WriteLine("Initialisation...");
 
-        Piece.InitShapes(); //il faudra absolument réinitialisé le tableau (car quand on recharge la page, cette méthode est appelé) 
+        Piece.InitShapes(); //Initialisation du tableau de Pieces. 
         
         delay = difficultyDelay;
+
+        isEnd = false; //on met isEnd à false (utile lors d'un retour en jeu via le bouton 'Retry' ou 'Play')
 
         score = new Score(choosenDifficulty);
 
         grid = new Grid(width, height);
+        
+        nextPieceGrid = new Grid(6, 6); //création d'une grille pour la prochaine pièce
+        nextPieceGrid.actualPiece = grid.nextPiece; //on définit la pièce actuelle de cette grille comme étant la prochaine pièce du jeu
+        nextPieceGrid.nextPiece = null; //il n'y a pas de prochaine pièce (puisque c'est la grille de la prochaine pièce)
+        nextPieceGrid.AddToGrid(-(grid.width/2)+2,0,0);
+
         Console.WriteLine(delay);
-
-        // Score.score = 0;
     }
-
-    // public static async Task Round() { //il faudra trouver une autre solution pour le Delay
-
-    //     Console.WriteLine("I launch the game !");
-
-    //     // var delay = 1000;
-
-    //     while(!isEnd) {
-    //         await Task.Delay(delay);
-
-    //         int offsetX = 0;
-    //         int offsetY = 1;
-    //         int offsetRotation = 0;
-
-    //         grid.RemovePiece(); //on enlève la pièce actuelle pour vérifier la nouvelle position (pour éviter les conflits).
-
-    //         bool isMovable = grid.VerifyNewPosition(offsetX, offsetY, offsetRotation);
-
-    //         if(!isMovable) {
-
-
-    //             Game.score.End(grid.actualPiece.y);
-
-    //             Game.delay = difficultyDelay;
-    //             Game.score.drop = "null";
-
-                
-    //             grid.AddToGrid(0, 0, 0); //on ajoute la pièce actuel à sa dernière position (avant de mettre la prochaine pièce)
-    //             grid.IsCompleteLine();
-
-    //             grid.actualPiece = grid.nextPiece;
-    //             grid.nextPiece = new Piece(grid.width, 2);
-
-    //             offsetY = 0;
-    //             offsetX = 0;
-
-    //             // Console.WriteLine("Fin de la pièce, début de la nouvelle");
-    //         } else {
-    //             offsetY = 1;
-    //             offsetX = 0;
-
-
-    //             // Console.WriteLine("nouvelle position pour la pièce !");
-    //             // Console.WriteLine("size : " + grid.pieces.Count);
-    //             // Console.WriteLine("x and y : " + grid.actualPiece.x + " " + grid.actualPiece.y);
-    //         }
-
-    //         //ici
-    //         bool isPosable = grid.VerifyNewPosition(offsetX, offsetY, offsetRotation);
-
-    //         if (isPosable){
-    //             grid.AddToGrid(offsetX, offsetY, offsetRotation); //on ajoute la pièce à sa nouvelle position.
-
-    //         } else {
-    //             isEnd = true; 
-    //             Console.WriteLine("HAHA NOOB YOU FAILLLLLLED");
-
-    //         }
-            
-
-    //     }
-    // }
 
     public static void Round() { //il faudra trouver une autre solution pour le Delay
 
@@ -128,6 +74,11 @@ public class Game {
                 grid.actualPiece = grid.nextPiece;
                 grid.nextPiece = new Piece(grid.width, 2);
 
+                nextPieceGrid.RemovePiece();
+                nextPieceGrid.actualPiece = grid.nextPiece;
+                nextPieceGrid.AddToGrid(-(grid.width/2)+2,0,0); //décalage sur le x : on enlève la taille de la grille/2 pour remettre à 0 les coordonnées, puis on ajoute 2 pour recentrer.
+                grid.actualPiece.x = grid.width/2; //On remet le x à la position central de la grille (modifier lors de l'affichage de la prochaine pièce)
+                
                 offsetY = 0;
                 offsetX = 0;
 
@@ -147,7 +98,6 @@ public class Game {
 
             if (isPosable){
                 grid.AddToGrid(offsetX, offsetY, offsetRotation); //on ajoute la pièce à sa nouvelle position.
-
             } else {
                 isEnd = true; 
                 Console.WriteLine("HAHA NOOB YOU FAILLLLLLED");
